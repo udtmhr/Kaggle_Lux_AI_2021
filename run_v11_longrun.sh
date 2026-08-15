b9580795ffd2f67c57bd275075e76c8ecd9bcdc1
udtmhr <tomhiro2002@gmail.com>
2026-08-15 17:22:58 +0900

    refactor: switch run script to 2GPU config and remove deprecated 1GPU configuration file

--- run_v11_longrun.sh
+++ run_v11_longrun.sh
@@ -3,7 +3,7 @@ set -e
 
 # v11 Long Run Script (5M steps, 2GPU)
 uv run --locked python run_monobeast.py \
-  --config-name survival_strategic_strength_v11_longrun_1gpu \
+  --config-name survival_strategic_strength_v11_longrun_2gpu \
   +load_dir=/home/ueda/workspace/Kaggle_Lux_AI_2021_Fork/outputs/survival_strategic/2026-08-15/12-02-58 \
   +checkpoint_file=100096_weights.pt \
   ++total_steps=5000000 \
